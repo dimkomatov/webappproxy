308f318aa4573c6d154e23bb34ef8957dde38a58
dimkomatov <dimkomatov@gmail.com>
2017-06-03 18:36:23 +0300

    +Testing

--- src/main/java/prox/ProxController.java
+++ src/main/java/prox/ProxController.java
@@ -9,8 +9,11 @@ import prox.dto.*;
 import prox.repository.AccessRepository;
 import prox.repository.StoreRepository;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -26,11 +29,23 @@ public class ProxController {
     Date date1 = null;
     Date date2 = null;
     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    FileWriter writer = new FileWriter("/opt/test/page", false);
+    FileWriter writer1 = new FileWriter("/opt/test/page1", false);
+    FileWriter writer2 = new FileWriter("/opt/test/page2", false);
+    FileWriter writer3 = new FileWriter("/opt/test/page3", false);
+    FileWriter writer4 = new FileWriter("/opt/test/page4", false);
+    FileWriter writer5 = new FileWriter("/opt/test/page5", false);
+    FileWriter writer6 = new FileWriter("/opt/test/page6", false);
+    FileWriter writer7 = new FileWriter("/opt/test/page7", false);
+
+    public ProxController() throws IOException {
+    }
 
     @RequestMapping(value = "/page")
     public Model getForm(Model model) throws ParseException {
-
+        Long start = System.currentTimeMillis();
         Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
         model.addAttribute("countAll", countAll);
 
         Integer countDistinct = accessRepository.countDistinct();
@@ -41,7 +56,17 @@ public class ProxController {
 
         List<RemotehostUrlAccess> remotehostUrl = accessRepository.findRemotehostUrl(10);
         model.addAttribute("remotehostUrl", remotehostUrl);
-
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer.append('\n');
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
         return model;
     }
 
@@ -49,7 +74,9 @@ public class ProxController {
     public Model getForm1(Model model,
                           @RequestParam(value = "dateCountryCity1", required = false) String dateCountryCity1,
                           @RequestParam(value = "dateCountryCity2", required = false) String dateCountryCity2) throws ParseException {
-
+        Long start = System.currentTimeMillis();
+        Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
         if (dateCountryCity1 != null || dateCountryCity2 != null)
             try {
                 date1 = formatter.parse(dateCountryCity1);
@@ -63,7 +90,17 @@ public class ProxController {
 
             List<CountryCount> countryCount = accessRepository.findCountryCount(date1, date2);
             model.addAttribute("countryCount", countryCount);
-
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer1.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer1.append('\n');
+            writer1.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
         return model;
     }
 
@@ -74,6 +111,9 @@ public class ProxController {
                           @RequestParam(value = "countryCoUrl", required = false) String countryCoUrl,
                           @RequestParam(value = "cityCoUrl", required = false) String cityCoUrl
     ) throws ParseException {
+        Long start = System.currentTimeMillis();
+        Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
 
         List<String> findAllCountry = accessRepository.findAllCountry();
         model.addAttribute("findAllCountry", findAllCountry);
@@ -98,6 +138,17 @@ public class ProxController {
         List<UrlCount> cityUrl = accessRepository.findUrlCity(cityCoUrl, date1, date2);
         model.addAttribute("cityUrl", cityUrl);
 
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer2.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer2.append('\n');
+            writer2.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
         return model;
     }
 
@@ -107,6 +158,9 @@ public class ProxController {
                           @RequestParam(value = "dateCountryCityHP2", required = false) String dateCountryCityHP2,
                           @RequestParam(value = "countryCoHP", required = false) String countryCoHP,
                           @RequestParam(value = "cityCoHP", required = false) String cityCoHP) throws ParseException {
+        Long start = System.currentTimeMillis();
+        Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
 
         List<String> findAllCountry = accessRepository.findAllCountry();
         model.addAttribute("findAllCountry", findAllCountry);
@@ -131,6 +185,17 @@ public class ProxController {
             List<UrlCount> cityHP = accessRepository.findHPCity(cityCoHP, date1, date2);
             model.addAttribute("cityHP", cityHP);
 
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer3.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer3.append('\n');
+            writer3.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
         return model;
     }
 
@@ -139,6 +204,10 @@ public class ProxController {
                           @RequestParam(value = "dateCountryCityAvgEl1", required = false) String dateCountryCityAvgEl1,
                           @RequestParam(value = "dateCountryCityAvgEl2", required = false) String dateCountryCityAvgEl2,
                           @RequestParam(value = "coAvgEl", required = false) String coAvgEl) throws ParseException {
+
+        Long start = System.currentTimeMillis();
+        Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
 
         List<String> findAllUrl = accessRepository.findAllUrl();
         model.addAttribute("findAllUrl", findAllUrl);
@@ -159,6 +228,17 @@ public class ProxController {
             List<UrlCount> cityAvgEl = accessRepository.findAvgElapseCity(coAvgEl, date1, date2);
             model.addAttribute("cityAvgEl", cityAvgEl);
 
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer4.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer4.append('\n');
+            writer4.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
         return model;
     }
 
@@ -168,6 +248,10 @@ public class ProxController {
                           @RequestParam(value = "dateClientStatistic1", required = false) String dateClientStatistic1,
                           @RequestParam(value = "dateClientStatistic2", required = false) String dateClientStatistic2,
                           @RequestParam(value = "rhClientStatistic", required = false) String rhClientStatistic) throws ParseException {
+
+        Long start = System.currentTimeMillis();
+        Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
 
         /*Client*/
         model.addAttribute("rhClient", rhClientStatistic);
@@ -202,6 +286,18 @@ public class ProxController {
             List<Url2Count> findClientUrlSumEl = accessRepository.findClientUrlSumEl(rhClientStatistic, date1, date2);
             model.addAttribute("findClientUrlSumEl", findClientUrlSumEl);
 
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer5.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer5.append('\n');
+            writer5.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
         return model;
     }
 
@@ -210,6 +306,10 @@ public class ProxController {
                           @RequestParam(value = "dateUrlStatistic1", required = false) String dateUrlStatistic1,
                           @RequestParam(value = "dateUrlStatistic2", required = false) String dateUrlStatistic2,
                           @RequestParam(value = "urlStatistic", required = false) String urlStatistic) throws ParseException {
+
+        Long start = System.currentTimeMillis();
+        Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
 
         List<String> findAllUrl = accessRepository.findAllUrl();
         model.addAttribute("findAllUrl", findAllUrl);
@@ -242,6 +342,18 @@ public class ProxController {
             List<Url2Count> findMostUrlIp = accessRepository.findMostUrlIp(urlStatistic, date1, date2);
             model.addAttribute("findMostUrlIp", findMostUrlIp);
 
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer6.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer6.append('\n');
+            writer6.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
         return model;
     }
 
@@ -250,6 +362,10 @@ public class ProxController {
     public Model getForm7(Model model,
                           @RequestParam(value = "dateAvgRhBytes1", required = false) String dateAvgRhBytes1,
                           @RequestParam(value = "dateAvgRhBytes2", required = false) String dateAvgRhBytes2) throws ParseException {
+
+        Long start = System.currentTimeMillis();
+        Integer countAll = accessRepository.countAll();
+        String countAllVal = countAll.toString();
 
         if (dateAvgRhBytes1 != null || dateAvgRhBytes2 != null)
             try {
@@ -262,6 +378,17 @@ public class ProxController {
             List<RhBytes> findAvgRhBytes = accessRepository.findAvgRhBytes(date1, date2);
             model.addAttribute("findAvgRhBytes", findAvgRhBytes);
 
+        Long end=System.currentTimeMillis() - start;
+        String text=end.toString();
+        try {
+            Date date = Calendar.getInstance().getTime();
+            writer7.write("Datetime/Time/Count DB: "+date+","+text + "," + countAllVal);
+            writer7.append('\n');
+            writer7.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
         return model;
     }
 
